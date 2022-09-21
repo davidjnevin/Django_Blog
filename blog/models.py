f29c7f296893af215b890a91a4cc0be9c49219c8
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-09-22 01:04:26 +0200

    Blog with email functionality

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -21,7 +22,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "PUBLISHED"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
@@ -45,3 +46,14 @@ class Post(models.Model):
     def __str__(self):
         """Return title."""
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ],
+        )
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,75 @@
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
-def post_detail(request, id):
-    """Return a specific post by 'id'."""
-    post = get_object_or_404(Post, id=id, status=Post.Status.Published)
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
 
-    return render(request, "blog/post/detail.html", {"post": post})
+    if request.method == "POST":
+        # form submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # All form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
+            send_mail(
+                subject, message, "your_account_to_send_email@gmail.com", [cd["to"]]
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
 
 
 def post_list(request):
     """Generate a list of all published posts."""
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Pagination using built in paginator
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    # posts = paginator.page(page_number)
+
     return render(request, "blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request, year, month, day, post):
+    """Return a specific post by 'id'."""
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+
+    return render(request, "blog/post/detail.html", {"post": post})
+
+
+class PostListView(ListView):
+    """Alternate post list views."""
+
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
